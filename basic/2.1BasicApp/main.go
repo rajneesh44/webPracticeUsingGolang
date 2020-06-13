c95388c12c8df708be4862f4b507da49e60cec43
rajneesh44 <rajneeshsharma338@gmail.com>
2020-06-14 04:09:36 +0530

    runner.conf, fresh, URLPATH added

--- basic/2.1BasicApp/main.go
+++ basic/2.1BasicApp/main.go
@@ -6,12 +6,29 @@ import (
 )
 
 // this will listen the webapp
-func handlerFunc(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "<h1>Welome to my awesome site</h1>")
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html") //specific to we request
+	//!! Browser renders the page as an html page.
+	fmt.Fprint(w, r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "Hey this is golang")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "Click here <a href = \"https://github.com/rajneesh44\"> abcdd </a>")
+	}
+	// r is the request we are sending
+	// w is the retrun response we get from the server
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)	  // nil is the builtin serve mux
+	http.ListenAndServe(":3000", nil) // nil is the builtin serve mux
 
 }
+
+// handler func takes repsone writer and request
+// w interfaces to the io
+
+// dyanmic reloading : automatically chaanges and reloading (not default)
+// downloaded "fresh package" Simply run the app using 1. go into the main directory where your files are there
+// $ fresh
+// fresh cant detect compiation error, so try to start server using go run file.go
